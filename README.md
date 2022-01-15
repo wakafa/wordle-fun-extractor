76c1218c383a22cdf6892e88e2d1374f017b8776
morco <mor.cohen1989@gmail.com>
2022-01-15 13:32:01 +0200

    merge from wrong named repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wordle-fun-extractor
 Work with English vocabulary in order to analyze wordle strategies
+
+Don't use if you enjoy playing Wordle and wanna keep doing so :S
--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from nltk.corpus import words  # need nltk.download() corpus -> words at first use
 import matplotlib.pyplot as plt
 import pandas as pd
+import random
 
 word_list = words.words()
 # prints 236736
@@ -41,22 +42,61 @@ def get_word_list_letter_count(words):
 data = dict(sorted(get_word_list_letter_count(five_letters_words).items()))
 
 import operator
-max = dict(sorted(data.items(), key=operator.itemgetter(1), reverse=True)[:10])
-print(max)
+top_ten = dict(sorted(data.items(), key=operator.itemgetter(1), reverse=True)[:10])
+print(f"Top 10 letters with occurences: : {top_ten}")
 
-t = []
 
-def is_contained_in_max(w):
+def is_contained_in_top(w, top=top_ten):
     for c in w:
-        if c not in max.keys():
+        if c not in top.keys():
             return False
     return True
 
-for w in five_letters_words:
-    if is_contained_in_max(w):
-        t.append(w)
+words_in_top_ten = []
 
-print(len(t))
-print(t)
+for w in five_letters_words:
+    if is_contained_in_top(w):
+        words_in_top_ten.append(w)
+
+
+# print(f"len(words_in_top_ten) words are composed of top ten letters only:" )
+# print(words_in_top_ten)
+
+
+def get_strat(top=15):
+    top_15 = dict(sorted(data.items(), key=operator.itemgetter(1), reverse=True)[:top])
+    strat_words = []
+    for i in range(2*len(five_letters_words)):
+        w = random.choice(five_letters_words)
+        if is_contained_in_top(w, top=top_15):
+            strat_words.append(w)
+            top_15_copy = dict(top_15)
+            for c in w:
+                top_15_copy.pop(c, None)
+            top_15 = top_15_copy
+    missing_letters = []
+    if len(top_15.keys()) > 0:
+        missing_letters = list(top_15.keys())
+    return strat_words, f"missing letters : {missing_letters}"
+    
+
+print(f"get a strat for 10: {get_strat(10)}")
+print(f"get a strat for 15: {get_strat()}")
+
+def greedy_strat():
+    print("Looking for a greedy strat")
+    strat, ml = get_strat(10)
+    while len(ml) > 22:
+        strat, ml = get_strat(10)
+    print(f"get a strat for 10: {strat, ml}")
+    strat, ml = get_strat(15)
+    while len(ml) > 22:
+        strat, ml = get_strat(15)
+    print(f"get a strat for 15: {strat, ml}")
+
+greedy = True
+if(greedy):
+    greedy_strat()
+# if you wanna see cool plot
 # plt.bar(data.keys(), data.values())
 # plt.show()
